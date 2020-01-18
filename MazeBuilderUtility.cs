4c49d93aa3d20d91dcedb267d5e8ab8ec59eca8e
Prof. Roger Crawfis <crawfis.3@osu.edu>
2020-01-18 12:21:32 -0500

    Flipped origin of CSV files to the lower-left.

--- MazeBuilderUtility.cs
+++ MazeBuilderUtility.cs
@@ -14,8 +14,28 @@ namespace CrawfisSoftware.Collections.Maze
             int width, height;
             List<Direction> directions;
             ReadCSVFile(filename, out width, out height, out directions);
-            return CreateMaze(width, height, directions, nodeAccessor, edgeAccessor);
+            Direction[,] directionGrid = MoveOriginToLowerLeft(width, height, directions);
+            return CreateMaze(directionGrid, nodeAccessor, edgeAccessor);
         }
+
+        public static Direction[,] MoveOriginToLowerLeft(int width, int height, List<Direction> directions)
+        {
+            Direction[,] directionGrid = new Direction[width, height];
+            int row = height - 1;
+            int column = 0;
+            foreach(Direction dir in directions)
+            {
+                directionGrid[column, row] = dir;
+                column++;
+                if(column>=width)
+                {
+                    column = 0;
+                    row--;
+                }
+            }
+            return directionGrid;
+        }
+
         public static Maze<int,int> CreateMaze(int width, int height, List<Direction> directions)
         {
             return MazeBuilderUtility<int,int>.CreateMaze(width, height, directions, DummyNodeValues, DummyEdgeValues);
@@ -37,6 +57,20 @@ namespace CrawfisSoftware.Collections.Maze
             }
             return mazeBuilder.GetMaze();
         }
+        public static Maze<N, E> CreateMaze(Direction[,] directions, GetGridLabel<N> nodeAccessor, GetEdgeLabel<E> edgeAccessor)
+        {
+            int width = directions.GetLength(0);
+            int height = directions.GetLength(1);
+            var mazeBuilder = new MazeBuilderExplicit<N, E>(width, height, nodeAccessor, edgeAccessor);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    mazeBuilder.SetCell(i, j, directions[i,j]);
+                }
+            }
+            return mazeBuilder.GetMaze();
+        }
 
         public static void ReadCSVFile(string filename, out int width, out int height, out List<Direction> directions)
         {
@@ -47,56 +81,50 @@ namespace CrawfisSoftware.Collections.Maze
             using (var file = new StreamReader(filename))
             {
                 string row = file.ReadLine();
-                string[] cells = row.Split(',');
-                width = cells.Length;
                 height = 1;
-                foreach (var cell in cells)
-                {
-                    int cellValue = Int32.Parse(cell);
-                    if (cellValue >= 0)
-                    {
-                        Direction dir = (Direction)Enum.Parse(typeof(Direction), cell);
-                        directions.Add(dir);
-                    }
-                    else
-                    {
-                        // Note: Undefined could also be the value of 16.
-                        directions.Add(Direction.Undefined);
-                    }
-                }
+                row = file.ReadLine();
+                ReadDirections(directions, row);
+                width = directions.Count;
                 row = file.ReadLine();
                 while (row != null)
                 {
                     height++;
-                    cells = row.Split(',');
-                    foreach (var cell in cells)
-                    {
-                        Direction dir;
-                        bool invalidInput = false;
-                        if (Enum.TryParse(cell, true, out dir))
-                        {
-                            // Parse will only return false if there is an invalid string (e.g. "N,NW"), not an invalid number (e.g, 234).
-                            if (Enum.IsDefined(typeof(Direction), dir) | dir.ToString().Contains(","))
-                            {
-                                directions.Add(dir);
-                            }
-                            else
-                            {
-                                invalidInput = true;
-                            }
-                        }
-                        else
-                        {
-                            invalidInput = true;
-                        }
-                        if (invalidInput)
-                        {
-                            //string error = string.Format("The value {0} being read in the CSV file {1} is not a valid set of Directions", dir, filename);
-                            //throw new InvalidCastException(error);
-                            directions.Add(Direction.Undefined);
-                        }
-                    }
+                    ReadDirections(directions, row);
                     row = file.ReadLine();
+                }
+            }
+        }
+
+        private static void ReadDirections(List<Direction> directions, string row)
+        {
+            string[] cells = row.Split(',');
+            foreach (var cell in cells)
+            {
+                Direction dir;
+                bool invalidInput = false;
+                // Convert cell's or format to a comma separated list.
+                string cellDirs = cell.Replace("|", ",");
+                if (Enum.TryParse(cellDirs, true, out dir))
+                {
+                    // Parse will only return false if there is an invalid string (e.g. "N,NW"), not an invalid number (e.g, 234).
+                    if (Enum.IsDefined(typeof(Direction), dir) | dir.ToString().Contains(","))
+                    {
+                        directions.Add(dir);
+                    }
+                    else
+                    {
+                        invalidInput = true;
+                    }
+                }
+                else
+                {
+                    invalidInput = true;
+                }
+                if (invalidInput)
+                {
+                    //string error = string.Format("The value {0} being read in the CSV file {1} is not a valid set of Directions", dir, filename);
+                    //throw new InvalidCastException(error);
+                    directions.Add(Direction.Undefined);
                 }
             }
         }
