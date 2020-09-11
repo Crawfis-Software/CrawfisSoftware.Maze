b26e5506c7427833257320ec7bbbb6a4ad5400bf
Roger Crawfis <crawfis.3@osu.edu>
2020-09-11 10:48:05 -0400

    Renamed LoopGenerator to LoopGeneratorSideWinder. A little clean-up

--- MazeBuilderRecursiveBacktracker.cs
+++ MazeBuilderRecursiveBacktracker.cs
@@ -17,7 +17,8 @@ namespace CrawfisSoftware.Collections.Maze
         /// <param name="height">The height of the desired maze</param>
         /// <param name="nodeAccessor">A function to retrieve any node labels</param>
         /// <param name="edgeAccessor">A function to retrieve any edge weights</param>
-        public MazeBuilderRecursiveBacktracker(int width, int height, GetGridLabel<N> nodeAccessor, GetEdgeLabel<E> edgeAccessor) : base(width, height, nodeAccessor, edgeAccessor)
+        public MazeBuilderRecursiveBacktracker(int width, int height, GetGridLabel<N> nodeAccessor = null, GetEdgeLabel<E> edgeAccessor = null) 
+            : base(width, height, nodeAccessor, edgeAccessor)
         {
         }
 
